f96973928bd9b238cbba13ab6bc676517104d46c
Adarsh Desai <desaiadarsh22@gmail.com>
2023-02-05 13:40:22 +0530

    Added state object for password options management

---
+++ src/js/model.js
@@ -0,0 +1,11 @@
+export const state = {
+  passwordStrength: "", // 0-3
+  passwordGenerated: "", // Generated from api
+  options: {
+    passwordLength: 0, // Length of password
+    allowUppercase: false,
+    allowLowercase: false,
+    allowNumbers: false,
+    allowSymbols: false,
+  },
+};
