40e781dc05f76120aec8793db3c5d60c3825fd93
Adarsh Desai <desaiadarsh22@gmail.com>
2023-02-10 13:04:08 +0530

    added setters for all varibles in state.options

--- src/js/model.js
+++ src/js/model.js
@@ -13,3 +13,19 @@ export const state = {
 export function updatePasswordLength(newLength) {
   state.options.passwordLength = newLength;
 }
+
+export function updateAllowUppercase(newValue) {
+  state.options.allowUppercase = newValue;
+}
+
+export function updateAllowLowercase(newValue) {
+  state.options.allowLowercase = newValue;
+}
+
+export function updateAllowNumbers(newValue) {
+  state.options.allowNumbers = newValue;
+}
+
+export function updateAllowSymbols(newValue) {
+  state.options.allowSymbols = newValue;
+}
