d2ae8cf46c27f567fc3a5a17df638615e5a16371
Adarsh Desai <desaiadarsh22@gmail.com>
2023-02-10 11:58:11 +0530

    Added function to print state

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,10 @@ import * as model from "./model.js";
 import formView from "./views/formView.js";
 import passwordView from "./views/passwordView.js";
 
+function printCurrentState() {
+  console.log(JSON.stringify(model.state, null, 2));
+}
+
 const controlLengthSlider = function (currentValue) {
   console.log(currentValue);
 };
