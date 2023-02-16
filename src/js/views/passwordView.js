0f24507fafe900694b60316eb4201b8f09a7fe35
Adarsh Desai <desaiadarsh22@gmail.com>
2023-02-16 19:45:09 +0530

    Added a function to render the new password to the UI

--- src/js/views/passwordView.js
+++ src/js/views/passwordView.js
@@ -1,6 +1,13 @@
 class PasswordView {
   renderGeneratedPassword(password) {
-    console.log(`renderGeneratedPassword: ${password}`);
+    const passwordDisplay = document.querySelector(".password__display");
+
+    if (passwordDisplay.classList.contains("password--placeholder")) {
+      console.log("placeholder");
+      passwordDisplay.classList.remove("password--placeholder");
+      passwordDisplay.classList.add("password--actual");
+    }
+    passwordDisplay.textContent = password;
   }
 }
 export default new PasswordView();
